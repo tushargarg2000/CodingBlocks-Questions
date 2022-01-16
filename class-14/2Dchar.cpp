f271691562fec996bd95e9b173e38263d7e3b0ec
aditya41 <35165537+aditya41@users.noreply.github.com>
2022-01-16 15:08:57 +0530

    Add files via upload

---
+++ class-14/2Dchar.cpp
@@ -0,0 +1,46 @@
+// 2Dchar.cpp
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	// int row;
+	// cin >> row;
+
+	// char arr[1000][1000];  //10^6;
+	// // char arr[10000000];
+	// for (int i = 0; i < row; i++) {
+	// 	cin >> arr[i];
+	// }
+	// for (int i = 0; i < row; i++) {
+	// 	cout << arr[i] << endl;
+	// }
+
+	// char arr[100];
+	// arr[0] = cin.get();
+	// cout << arr[0];
+
+	// for (int i = 0; i < 10; i++) {
+	// 	arr[i] = cin.get();
+	// }
+
+	// cout << arr;
+
+	char arr[100][100];
+
+	int n;
+	cin >> n;
+
+	cin.ignore();
+
+	for (int i = 0; i < n; i++) {
+		cin.getline(arr[i], 100);
+	}
+
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << endl;
+	}
+
+
+}
